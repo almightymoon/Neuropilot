1569ed5b2644ea30077befeddf12e13096679b5e
= <your-email@example.com>
2025-08-01 20:02:22 +0500

    links updated

--- apps/web-client/.next/static/chunks/webpack.js
+++ apps/web-client/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "acd5074ec8a33f53"; }
+/******/ 		__webpack_require__.h = function() { return "68c01eaecd6e9f93"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
